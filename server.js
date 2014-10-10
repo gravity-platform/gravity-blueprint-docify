dbab0c279856032ef49f78f2e86e3793cdce1686
Lucas Bickel <hairmare@purplehaze.ch>
2014-10-10 07:32:22 +0200

    more output

--- server.js
+++ server.js
@@ -11,9 +11,17 @@ connect()
   .use(function(req, res) {
     if (req.url == '/github.hook') {
 
-      console.log("Updating " + req.body.repository.full_name);
-      var update = child_process.spawn("npm", ["update", req.body.repository.full_name]);
+      console.log("Updating " + req.body.repository.name);
+      var update = child_process.spawn("npm", ["update", req.body.repository.name], { cwd: __dirname });
+
+      update.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+      });
+      update.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+      });
       update.on('close', function(code) {
+        console.log('update process exited with code ' + code);
         var build = child_process.fork(__dirname + "/build.js");
         build.on('close', function(code) {
           console.log('build process exited with code ' + code);
